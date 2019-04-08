3111985f4a3ceefd0890ab6e6316f63b54159c7c
tenusha <tenushamadhushan@gmail.com>
2019-04-08 21:25:58 +0530

    index.js updated

--- services/index.js
+++ services/index.js
@@ -1,10 +1,15 @@
 'use strict'
-;
+    ;
 const express = require('express');
 const app = express();
 const login = require('./routers/login');
 
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(login);
 
